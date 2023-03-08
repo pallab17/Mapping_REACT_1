88c94d2152ef9463f6f1160c636a2d06522008b7
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-08 19:05:21 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,50 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+// pall argument or side dish r kache contacts pg ta ache
+// i.e. pall is acting as contacts pg or contacts array
+// i.e. contacts.name === pall.id 
+function createCard(pall){
+  return (
+    <Card
+    key={pall.id}
+    name={pall.name}
+    img={pall.imgURL}
+    tel={pall.phone}
+    email={pall.email}
+  />
+
+  );
+
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+{/* 
+  bar bar jodi card component ta ke likhi 
+  tahole time waste aar secondly amra dekhte pacchi je 
+  contacts pg r array ta traverse hocche ekta kore element 
+  tahole jodi amra for loop type r ekta loop chaliye di then
+  ei kaj ta automatically hobe mane traverse ta uatomatically hobe
 
-      <Card
+
+map r syntax hocche
+arrayname.map(function_name of the function jetate amra 
+  code likhbo ekta card r jonno aar baki joto gulo card ache contact pg e ota traverse kore 
+  debe map function automatically
+  map fn === for loop/ any other loop fn )
+ 
+  imp note is while using map
+  ekta unique key lagbe like fingerprint password
+   key={contact.id} 
+   ekhane eita hocche se password
+
+*/}
+     {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,7 +62,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
